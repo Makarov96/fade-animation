431d8d9b386ea03b57c164a43d3fe106e9cccf50
Makarov96 <estev97@gmail.com>
2023-07-27 22:53:23 -0700

    Initial Commit

---
+++ sticky-header/sticky_headerApp.swift
@@ -0,0 +1,17 @@
+//
+//  sticky_headerApp.swift
+//  sticky-header
+//
+//  Created by Guerin Steven Colocho Chacon on 27/07/23.
+//
+
+import SwiftUI
+
+@main
+struct sticky_headerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
